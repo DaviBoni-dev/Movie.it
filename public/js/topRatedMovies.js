f8dce8c48c284fad1f4b0f667448abff5e701492
Davi Boni <bonidavi07@gmail.com>
2024-12-23 19:49:20 -0300

    Inteligência do menu responsivo criada

--- public/js/topRatedMovies.js
+++ public/js/topRatedMovies.js
@@ -26,3 +26,29 @@ async function updatePage() {
 const buttonLoadMore = document.getElementById("toprated-loadmore-button");
 
 buttonLoadMore.addEventListener("click", updatePage);
+
+// Seleciona o botão e o menu
+const menuToggle = document.getElementById('menu-toggle');
+const headerRight = document.getElementById('header-right');
+const headerLeft = document.getElementById('header-left');
+const headerList = document.getElementById('header-list');
+
+let isClose = true;
+// Alterna a classe 'open' ao clicar no botão
+menuToggle.addEventListener('click', () => {
+if(isClose){
+  headerRight.style.display = "none";
+  headerLeft.style.display = "none";
+  headerList.style.display = "flex";
+  headerList.classList.add('open')
+  isClose = false;
+} else {
+  headerRight.style.display = "flex";
+  headerLeft.style.display = "flex";
+  headerList.style.display = "none";
+  headerList.classList.remove('open')
+  isClose = true;
+}
+  // console.log("cliquei");
+  // headerRight.classList.toggle('open');
+});
